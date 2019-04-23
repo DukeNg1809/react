c3a23210137ed35765867537285884e8c814d39d
DukeNg1809 <41763962+DukeNg1809@users.noreply.github.com>
2019-04-24 03:11:08 +1000

    a

---
+++ src/api.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+const API_KEY = "6a630147b3904915a3146c7c2cd1a47d";
+export function useNewsArticles() {
+  const [loading, setLoading] = useState(true);
+  const [headlines, setHeadlines] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    getHeadlines()
+      .then(headlines => {
+        setHeadlines(headlines);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    loading,
+    headlines,
+    error
+  };
+}
+function getHeadlines() {
+  const url = `https://newsapi.org/v2/top-headlines?country=au&apiKey=${API_KEY}`;
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(res => res.articles)
+    .then(articles =>
+      articles.map(article => ({ title: article.title, url: article.url }))
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
+import { checkPropTypes } from "prop-types";
+import { useNewsArticles } from "./api";
+
+function Headline(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
 
 function App() {
+  const { loading, headlines, error } = useNewsArticles();
+  if (loading) {
+    return <p>loading...</p>;
+  }
+  if (error) {
+    return <p>Something went wrong: {error.message}</p>;
+  }
   return (
     <div className="App">
-      <h1>Hello CAB230</h1>
-      <h2>Welcome to React!</h2>
+      {headlines.map(headline => (
+        <Headline title={headline.title} key={headline.url} />
+      ))}
     </div>
   );
 }
